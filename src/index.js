47f653775ee90b007a8cdcdc8f14e9bfee8b3ecb
David Cantrell <foo@foo.com>
2017-11-26 15:42:58 -0600

    added fetch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-/* eslint-disable no-console */
+import {getUsers} from './api/userApi';
 
-import './index.css';
+getUsers().then(result => {
+    let usersBody = "";
 
-import numeral from 'numeral';
+    result.forEach(user => {
+        usersBody += `<tr>
+            <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+        </tr>`
+    });
 
-const courseValue = numeral(1000).format('$0,0.00');
-// debugger;
-console.log(`I would pay ${courseValue} for this course!`);
+    global.document.getElementById('users').innerHTML = usersBody;
+});
--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,21 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-    it('should say hello', (done) => {
+    it('should have h1 that says Users', (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(index, function(err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hello!");
+            expect(h1.innerHTML).to.equal("Users");
+            done();
+            window.close();
+        });
+    });
+
+    it('should have table with tbody id=users', (done) => {
+        const index = fs.readFileSync('./src/index.html', 'utf-8');
+        jsdom.env(index, function(err, window) {
+            const tbody = window.document.getElementById('users');
+            expect(tbody).to.not.be.null;
             done();
             window.close();
         });
